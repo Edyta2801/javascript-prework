e2db720bbe3f589a2c8fe0fba6e2d250ef81defa
Edyta Szarowska <edyta.szarowska@interia.pl>
2019-07-19 11:46:12 +0200

    add function getMoveName

--- js/script.js
+++ js/script.js
@@ -2,15 +2,33 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
-    computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier';
-} else if (randomNumber == 3) {
-    computerMove = 'nożyce';
+// let computerMove = 'nieznany ruch';
+
+// if (randomNumber == 1) {
+//     computerMove = 'kamień';
+// } else if (randomNumber == 2) {
+//     computerMove = 'papier';
+// } else if (randomNumber == 3) {
+//     computerMove = 'nożyce';
+// }
+
+function getMoveName(argMoveId) {
+    if (argMoveId == 1) {
+        return 'kamień';
+    }
+    else if (argMoveId == 2) {
+        return 'papier';
+    }
+    else if (argMoveId == 3) {
+        return 'nożyce'
+    }
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
 }
+
+
 printMessage('Mój ruch to: ' + computerMove);
 
 
